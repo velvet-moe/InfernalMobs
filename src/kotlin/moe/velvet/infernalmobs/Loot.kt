5a0f8e947e5a7e425dd5cf17c6756c5ab70ee35f
Checksum <checksum@velvet.moe>
2022-07-07 20:25:19 -0500

    revert: Code cleanup shenanigans

--- src/kotlin/moe/velvet/infernalmobs/Loot.kt
+++ src/kotlin/moe/velvet/infernalmobs/Loot.kt
@@ -43,7 +43,10 @@ fun createLoot(type: Loot, amount: Int = 1): ItemStack {
     //getInstance().logger.info(type.lore.toString())
     meta.lore(type.lore)
     if (type.type != LootType.Item) meta.persistentDataContainer.set(NamespacedKey(getInstance(), "loot-type"), PersistentDataType.STRING, type.id.lowercase())
-    item.itemMeta = meta
+
+    @Suppress("UsePropertyAccessSyntax")
+    // TODO: Insert explanation of why this is necessary
+    item.setItemMeta(meta)
     return item
 }
 
